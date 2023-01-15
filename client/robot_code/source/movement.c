3850090cacfd3675ac774315b1daab7528b2efd9
Jordan Allemand <vylaxez@gmail.com>
2023-01-15 15:07:37 +0100

    New features
    implemented Ray structure to store the sonar sensor data in utils.c
    implemented List structure to dynamically store Ray instances in utils.c (needs to be tested)
    implemented collect_and_store_ray method in movement.c to abstract the process and make it more clear
    modified turn_robot method to return a List and perform rays collection only if passed a second argument scan != 0

--- client/robot_code/source/movement.c
+++ client/robot_code/source/movement.c
@@ -3,6 +3,7 @@
 #include "ev3.h"
 #include "ev3_port.h"
 #include "ev3_tacho.h"
+#include "utils.c"
 
 #ifndef __ARM_ARCH_4T__
     /* Disable auto-detection of the brick (you have to set the correct address below) */
@@ -22,10 +23,48 @@
 //Constants declaration
 #define WHEEL_RADIUS 27.5
 #define PI 3.142857
+
 #define left_wheel_port 66
 #define right_wheel_port 67
 
-void turn_robot(int angle, int scan=0) {
+/*TODO:
+= create a data structure to store rays data
+= implement a dynamic array structure (List) because we can't tell for sure how many cycles the rotation of the robot can take (due to inconsistencies caused partly by the acceleration phase of motors)
+- (optionnal) implement optimized operation for list such as storing the closest ray index
+= collect rays at a given frequency
+- be able to identify symetry groups in the collected ray data (need some experimentation to figure out the symetry groups of the ball, enemy robot and walls)
+- identify the rays forming a symetry group
+- (optionnal) apply some mapping transformation (interpolation, extrapolation, ...) in order to compensate for the acceleration phase of the motors ???
+- pick the median ray index from within the rays subset
+- get the corresponding angle (knowing that the robot angle isn't the same as it was when the sweep started)
+- rotate the robot back to the chosen direction 
+*/
+
+/// @brief Collect Ray data and store it in the List given as parameter
+void collect_and_store_ray(struct List* list) {
+    struct Ray ray;
+
+    int angle = 0; //TODO: use the compass value here
+    float distance = 0.0; //TODO: implement something like "get_distance()" in sensors.c that gets the value from the sonar sensor
+
+    initRay(&ray, distance, angle);
+    put(&list, &ray);
+}
+
+void put(struct List* list, int value)
+
+/// @brief turns the robot of a given angle and (optionaly) collect rays data
+/// @param angle angle (in degrees) of the sweep
+/// @param scan if != 0, rays data will be collected
+struct List turn_robot(int angle, int scan=0) {
+    
+    //initialise rays collection (if we don't scan we'll just return an empty list and ignore its value anyways)
+    struct List raysList;
+    init(&raysList);
+
+    //Collect and store the initial ray
+    if (scan) collect_and_store_ray(&raysList);
+
     // Set the motors to "coast" mode
     set_tacho_stop_action_inx(left_wheel_port, TACHO_COAST);
     set_tacho_stop_action_inx(right_wheel_port, TACHO_COAST);
@@ -44,15 +83,20 @@ void turn_robot(int angle, int scan=0) {
     set_tacho_command_inx(right_wheel_port, TACHO_RUN_TO_REL_POS);
 
     // Wait for the motors to finish
-    while (get_tacho_state(left_wheel_port) != TACHO_STATE_HOLD) {
-        Sleep(10);
+    while (get_tacho_state(left_wheel_port) != TACHO_STATE_HOLD && get_tacho_state(right_wheel_port) != TACHO_STATE_HOLD) {
+        //Collect and store the current ray
+        if (scan) collect_and_store_ray(&raysList);
+        
+        Sleep(10);//TODO: change this to the ray collecting period
     }
-    while (get_tacho_state(right_wheel_port) != TACHO_STATE_HOLD) {
-        Sleep(10);
-    }
+
+    //Collect and store the final ray
+    if (scan) collect_and_store_ray(&raysList);
+
+    return raysList;
 }
 
-int main( void )
+int main( void ) //TODO: this method is just for testing, the turn_method should only be called from the main.c script in the final version
 {
     if ( ev3_init() == -1 ) return ( 1 );
 
@@ -62,6 +106,6 @@ int main( void )
 
     int angle = 90; //example
 
-    turn_robot(angle,left_wheel_port,right_wheel_port);
+    (void) turn_robot(angle); //note: the void casting is unnecessary but more "ethical" 
     return 0;
 }
