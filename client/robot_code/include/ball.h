ac57053daef29577cc2cab3dc03fab1f44dca7cc
ROBERTO SIRIGU s303346 <s303346@studenti.polito.it>
2023-01-28 13:54:41 +0100

    Removed some old prototypes

--- client/robot_code/include/ball.h
+++ client/robot_code/include/ball.h
@@ -1,11 +1,5 @@
-#include "ev3_tacho.h"
+#include "ev3.h"
 
-int initialize_arm(int servo_port, uint8_t *sn_arm);
-
-int initialize_hand(int dc_port, uint8_t *sn_hand);
-
-int shoot_ball(uint8_t sn_arm);
-
-int grab_ball(uint8_t sn_hand);
-
-int reload(uint8_t sn_hand);
+int shoot_ball(uint8_t* components);
+int grab_ball(uint8_t* components);
+int reload(uint8_t* components);
