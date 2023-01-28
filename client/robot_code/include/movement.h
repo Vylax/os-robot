e8fff27c98adb70e1cbbe91f102d08e4552f38f8
ROBERTO SIRIGU s303346 <s303346@studenti.polito.it>
2023-01-28 19:20:34 +0100

    Minor fix

--- client/robot_code/include/movement.h
+++ client/robot_code/include/movement.h
@@ -1,14 +1,14 @@
 /// @brief Collect Ray data and store it in the List given as parameter
-void collect_and_store_ray(struct List* list, uint8_t* components);
+void collect_and_store_ray(List* list, uint8_t* components);
 
 /// @brief turns the robot of a given angle and (optionaly) collect rays data
 /// @param angle angle (in degrees) of the sweep
 /// @param scan if != 0, rays data will be collected
-struct List turn_robot(int angle, int scan, uint8_t* components);
+List turn_robot(int angle, int scan, uint8_t* components);
 
 /// @brief Identify a ball position, and turn the robot towards it <br/> Returns the distance of the robot center of mass from the ball or -1 if no ball was found
 /// @param raysList 
-int turn_to_ball(struct List* raysList, uint8_t* components);
+int turn_to_ball(List* raysList, uint8_t* components);
 
 /// @brief keeps two motors moving forever
 void move_forever(int speed_sp, uint8_t* components);
