422430d49ddaa0a55653fbc628d474940e3dc5bb
ROBERTO SIRIGU s303346 <s303346@studenti.polito.it>
2023-01-28 13:55:38 +0100

    Removed some old prototypes

--- client/robot_code/include/sensors.h
+++ client/robot_code/include/sensors.h
@@ -1,20 +1,13 @@
-/* TODO fix the includes if nec. */
 #include "ev3.h"
-#include "ev3_port.h"
-#include "ev3_sensor.h"
 
-
-/* init/uninit functions */
-void init_sensors(uint8_t** ports);
-void uninit_sensors(uint8_t** ports);
 /* get value from sensor */
-int get_value_sonar(uint8_t port);
-int get_value_gyro_ang(uint8_t port);
-int get_value_gyro_rate(uint8_t port);
-int get_value_color(uint8_t port);
-int get_value_touch(uint8_t port);
-int get_value_compass(uint8_t port);
+int get_value_sonar(uint8_t component);
+int get_value_gyro_ang(uint8_t component);
+int get_value_gyro_rate(uint8_t component);
+int get_value_color(uint8_t component);
+int get_value_touch(uint8_t component);
+int get_value_compass(uint8_t component);
 /* reset sensor (where needed) */
-void reset_sonar(uint8_t port);
-void reset_gyro(uint8_t port);
-void reset_color(uint8_t port);
+void reset_sonar(uint8_t component);
+void reset_gyro(uint8_t component);
+void reset_color(uint8_t component);
