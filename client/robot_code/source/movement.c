1e075e07ecd073b89fc11afc1108b973c5344989
Roby603 <s303346@studenti.polito.it>
2023-01-31 20:24:03 +0100

    Debugging

--- client/robot_code/source/movement.c
+++ client/robot_code/source/movement.c
@@ -77,11 +77,10 @@ List turn_robot(int angle, int scan) {
     get_tacho_state(components[LEFT_MOTOR], state_left, (size_t)20);
     get_tacho_state(components[RIGHT_MOTOR], state_right, (size_t)20);
 
-    printf("state_left: %s\n", state_left);
-    return;
     // Wait for the motors to finish
-    while (state_left != TACHO_HOLDING || state_right != TACHO_HOLDING) {
+    while (state_left != "TACHO_HOLDING" || state_right != "TACHO_HOLDING") {
         // Collect and store the current ray
+        printf("state_left: %s\n", state_left);
         if (scan) collect_and_store_ray(&raysList);
         
         // Wait for POLLING_RATE ms before polling again
