fb47773f7b5fb71dfbdf224ba22e5ec8e6e7a3a9
zhengqi <qin.zheng@eurecom.fr>
2023-02-01 23:05:05 +0100

    debug

--- client/robot_code/source/main.c
+++ client/robot_code/source/main.c
@@ -105,8 +105,7 @@ void attacker()
     // 1)from the start position, move forward for 65cm 
     printf("1 ");
     t1 = cal_run_time(650, speed_forward);
-    move_timed(speed_forward, t1);
-    printf("waiting for %.4f secs", t1);
+    move_timed(speed_forward, 2000);
     
     // 2)turn right to +90 
     printf("2 \n");
